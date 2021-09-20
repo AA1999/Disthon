d25a4f07a9e4fb55a00ba0aae99808be46f8e7dc
sebkuip <kuipers.sebastian@ziggo.nl>
2021-09-20 12:56:21 +0200

    Added PyPi files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_des = file.read()
+
+setup(
+    name="disthon",
+    packages=["disthon",],
+    install_requires=[],
+    description="An API wrapper for the discord API written in python",
+    version="x.x.x",
+    long_description=long_des,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Arshia",
+    author_email="xxx",
+    url="https://github.com/AA1999/Disthon",
+    keywords=["API", "discord"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+    ]
+)
