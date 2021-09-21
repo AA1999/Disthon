63bd931e512fae37a82c7d0579acf58191792eae
crime05 <dcthompson79@outlook.com>
2021-09-20 20:36:42 -0400

    Started guild.py

--- disthon/guild.py
+++ disthon/guild.py
@@ -3,17 +3,6 @@ import unicodedata
 import datetime
 import copy
 
-class Guild(Hashable):
-    """Returns a discord Guild.
-        
-        
-        Often referred to as a server, and is referred to as a server in the official Discord UI
-        
-        
-        represents: x == y:
-          Checks if two guilds are equal."""
-    
-
 __slots__(
     'region'
     'owner_id'
@@ -26,13 +15,25 @@ __slots__(
     '_banner'
 )
 
+class Guild(Hashable):
+    """Returns a discord Guild.
+        
+        
+        Often referred to as a server, and is referred to as a server in the official Discord UI
+        
+        
+        represents: x == y:
+          Checks if two guilds are equal."""
+    def __init__(self):
+      "Guild Class"
+
+
+    def _add_channel(self, channel: GuildChannel,/) -> None:
+     self._channels[channel.id] = channel    
+
+
+
 
 
 
 #properties
-def __init__(self):
-    "Guild Class"
-
-
-def _add.channel(self, channel: GuildChannel,/) -> None:
-self._channels[channel.id] = channel
