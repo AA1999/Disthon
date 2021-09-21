66fa956922cb912178bc82373394c99823a58272
crime05 <dcthompson79@outlook.com>
2021-09-21 17:15:03 -0400

    Updated guild.py, cleaned up imports

--- disthon/guild.py
+++ disthon/guild.py
@@ -1,11 +1,19 @@
-from typing import Hashable
+import typing
+from typing import (
+  Hashable,
+  Dict,
+  NamedTuple,
+  Optional,
+  Snowflake
+  )
 import unicodedata
 import datetime
 import copy
 
+
 __slots__(
     'region'
-    'owner_id'
+    'owner.id'
     'mfa.level'
     'name'
     'id'
@@ -14,6 +22,21 @@ __slots__(
     '_vanity'
     '_banner'
 )
+
+class BanEntry(NamedTuple):
+  user: User
+  reason: Optional[str]
+
+class GuildLimit(NamedTuple):
+  filesize: int
+  emoji: int
+  channels: int
+  roles: int
+  categories: int
+  bitrate: int
+  stickers: int
+
+
 
 class Guild(Hashable):
     """Returns a discord Guild.
@@ -24,12 +47,29 @@ class Guild(Hashable):
         
         represents: x == y:
           Checks if two guilds are equal."""
-    def __init__(self):
-      "Guild Class"
+    def __init__(self, datas: GuildPayload):
+      self._members: Dict[int, Member] = {}
+      self._channels: Dict[int, GuildChannel] = {}
+
+      
 
 
     def _add_channel(self, channel: GuildChannel,/) -> None:
-     self._channels[channel.id] = channel    
+     self._channels[channel.id] = channel  
+
+
+    def _delete_channel(self, channel: Snowflake) -> None:
+      self._channels.pop(channel.id, None)
+
+    def add_member(self, member: Member) -> None:
+      self._members[member.id] = member
+
+    def add_roles(self, role: Role) -> None:
+      for p in self._roles.values:
+        p.postion += not p.is_default()
+        #checks if role is @everyone or not
+
+      self._roles[role.id] = role
 
 
 
