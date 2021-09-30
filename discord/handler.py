a012e140e288c5c0ad8270cf6acbf42cf3ea6fe6
sebkuip <kuipers.sebastian@ziggo.nl>
2021-09-30 16:21:59 +0200

    Token header is now automatically added

--- discord/handler.py
+++ discord/handler.py
@@ -5,7 +5,7 @@ import aiohttp
 class Handler:
     def __init__(self):
         self.base_url: str = 'https://discord.com/api/v9/'
-        self.user_agent: str = "Disthon test library V0.0.1b. User: sebkuip#3632"
+        self.user_agent: str = "Disthon test library V0.0.1b"
 
     async def request(self, method: str, dest: str, *, headers: typing.Optional[dict] = None, data: typing.Optional[dict] = None) -> typing.Union[str, dict]:
         async with self.__session.request(method, self.base_url + dest, headers=headers, json=data) as r:
@@ -22,18 +22,18 @@ class Handler:
             return text
 
     async def login(self, token: str) -> dict:
-        self.__session = aiohttp.ClientSession()
+        self.__session = aiohttp.ClientSession(headers={"Authorization": "Bot " + self.token})
         self.token = token
 
         try:
-            auth_data = await self.request("GET", "/users/@me", headers={"Authorization": "Bot " + self.token})
+            auth_data = await self.request("GET", "/users/@me")
         except ConnectionError as e:
             raise ConnectionError("The token passed is invalid") from e
 
         return auth_data
 
     async def gateway(self) -> str:
-        gw_data = await self.request("GET", "/gateway/bot", headers={"Authorization": "Bot " + self.token})
+        gw_data = await self.request("GET", "/gateway/bot")
         url = gw_data['url'] + '?encoding=json&v=9&compress=zlib-stream'
         return url
 
@@ -56,7 +56,7 @@ class Handler:
             'content': content,
         }
 
-        data = await self.request("POST", f"/channels/{channel_id}/messages", headers={"Authorization": "Bot " + self.token}, data=payload)
+        data = await self.request("POST", f"/channels/{channel_id}/messages", data=payload)
         try:
             if data['code'] == 50008:
                 raise TypeError("Invalid channel")
@@ -66,12 +66,12 @@ class Handler:
             return data
 
     async def fetch_channel(self, channel_id: int):
-        data = await self.request("GET", f"/channels/{channel_id}", headers={"Authorization": "Bot " + self.token})
+        data = await self.request("GET", f"/channels/{channel_id}")
         return data
 
     async def edit_guild_text_channel(self, channel_id: int, **options):
         payload = {k: v for k, v in options.items()}
-        await self.request("PATCH", f"/channels/{channel_id}", headers={"Authorization": "Bot " + self.token, 'Content-Type': 'application/json'}, data=payload)
+        await self.request("PATCH", f"/channels/{channel_id}", headers={'Content-Type': 'application/json'}, data=payload)
 
     async def edit_guild_voice_channel(self, channel_id: int, **options: typing.Any):
         payload = {
@@ -83,4 +83,4 @@ class Handler:
             'parent_id': options['category'],
             'rtc_region': options['region'],
         }
-        await self.request("PATCH", f"/channels/{channel_id}", headers={"Authorization": "Bot " + self.token}, data=payload)
+        await self.request("PATCH", f"/channels/{channel_id}", data=payload)
