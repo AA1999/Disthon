ebf904078b732bbf507a9b8c9bc47c7d5c37f6e8
crimes15 <82011615+wooHuncho@users.noreply.github.com>
2021-10-22 20:01:24 -0400

    Updated imports, and started classes

--- discord/channels/guildchannel.py
+++ discord/channels/guildchannel.py
@@ -1,5 +1,18 @@
+from typing import Optional
+from Disthon.discord.types.snowflake import Snowflake
 from discord.channels.basechannel import BaseChannel
+from discord.guild.guild import GuildChannel
+import discord.abc
 
 
-class GuildChannel(BaseChannel):
-    pass
+class TextChannel(discord.abc.GuildChannel):
+    __slots__ = (
+        "name",
+        "id",
+        "guild",
+        "nsfw",
+        "category_id",
+        "position"
+    )
+
+  
