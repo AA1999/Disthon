7eea4ee0d2df25db2c146a8b1eff9b90629c8e52
crimes15 <82011615+wooHuncho@users.noreply.github.com>
2021-11-06 01:58:20 -0400

    Added a mention property

--- discord/channels/basechannel.py
+++ discord/channels/basechannel.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 from ..abc.discordobject import DiscordObject
 from ..types.snowflake import Snowflake
-
-
+from ..utils.datetime import 
 class BaseChannel(DiscordObject):
-    __slots__ = ('_id', '_name')
+    __slots__ = ('_id', '_name', '_mention')
 
     _id: Snowflake
     _name: str
@@ -17,4 +16,12 @@ class BaseChannel(DiscordObject):
     @property
     def name(self):
         return self._name
+    
+    @property
+    def mention(self):
+        return f"<#{self._id}>"
+    
+    @property
+    def created_at(self):
+       return
 
