91c8d748cfd41dced4be92b0f73aba55ba3dde46
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-27 16:00:52 +0530

    Made Snowflake a subclass of int

--- discord/types/snowflake.py
+++ discord/types/snowflake.py
@@ -1,44 +1,19 @@
-from __future__ import annotations
-
-from pydantic import BaseModel
-
 __all__ = ("Snowflake",)
 
 
-class Snowflake(BaseModel):
-    id: int
+class Snowflake(int):
+    @property
+    def timestamp(self):
+        return (self >> 22) + 1420070400000
 
-    def __eq__(self, other) -> bool:
-        if isinstance(other, (int, str)):
-            return self.id == other
-        return isinstance(other, Snowflake) and self.id == other.id
+    @property
+    def worker_id(self):
+        return (self & 0x3E0000) >> 17
 
-    def __ne__(self, other: object) -> bool:
-        return not self.__eq__(other)
+    @property
+    def process_id(self):
+        return (self & 0x1F000) >> 12
 
-    def __lt__(self, other: object):
-        if isinstance(other, int):
-            return self.id < other
-        if isinstance(other, str) and other.isdigit():
-            return self.id < int(other)
-        if isinstance(other, Snowflake):
-            return self.id < other.id
-        raise NotImplementedError
-
-    def __le__(self, other: object):
-        return self.__eq__(other) or self.__lt__(other)
-
-    def __gt__(self, other: object):
-        return not self.__le__(other)
-
-    def __ge__(self, other: object):
-        return not self.__lt__(other)
-
-    def __str__(self) -> str:
-        return str(self.id)
-
-    def __int__(self) -> int:
-        return int(self.id)
-
-    def __repr__(self) -> str:
-        return f"Snowflake with id {self.id}"
+    @property
+    def increment(self):
+        return self & 0xFFF
