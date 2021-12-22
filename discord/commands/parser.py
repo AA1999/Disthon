0c91c96b4d5665ec50f0ff1017003eed18c9a6ff
Rashaad <rashaadzma@gmail.com>
2021-12-22 10:02:59 +0530

    Improved CommandParser

--- discord/commands/parser.py
+++ discord/commands/parser.py
@@ -1,37 +1,66 @@
-from re import L
-from typing import List, Dict, Tuple, Optional
+import inspect
+import re
+from typing import List, Dict, Tuple, Optional, Union, Any
 
 from discord.message import Message
+from . import Command
 from .core import Command
 
 
 class CommandParser:
     def __init__(
-        self,
-        command_prefix: str,
-        commands: Dict[str, Command],
-        case_sensitive: bool = True,
+            self,
+            command_prefix: str,
+            commands: Dict[str, Command],
+            case_sensitive: bool = True,
     ):
         self.command_prefix = command_prefix
         self.case_sensitive = case_sensitive
         self.commands = commands
 
-    def remove_prefix(self, content):
+    def remove_prefix(self, content: str):
         command_prefix = self.command_prefix
         if isinstance(command_prefix, (tuple, list, set)):
             for prefix in self.command_prefix:
                 if content.startswith(prefix):
-                    return content[len(prefix) :]
+                    return content[len(prefix):]
 
         elif isinstance(command_prefix, str):
-            return content[len(command_prefix) :]
+            return content[len(command_prefix):]
 
-    def parse_message(self, message: Message) -> Tuple[Optional[Command], List[str]]:
+    def get_args(self, command: Command, content: str, prefix=None):
+        prefix = prefix or command.name
+        content = content[len(prefix):].strip()
+        signature = inspect.signature(command.callback)
+        parameters = signature.parameters.copy()
+        parameters.pop(tuple(parameters.keys())[0])  # Remove the message parameter
+
+        args = content.split()
+        positional_arguments = []
+        kwargs = {}
+        extra_kwargs = {}
+
+        for index, (name, parameter) in enumerate(parameters.items()):
+            if parameter.kind is parameter.KEYWORD_ONLY:
+                kwargs[name] = (" ".join(args[index:])).strip()
+
+            elif parameter.kind is parameter.VAR_KEYWORD:
+                extra_kwargs = dict(re.findall(r"(\D+)=(\w+)", content))
+
+            elif parameter.kind is parameter.VAR_POSITIONAL:
+                positional_arguments.extend(args)
+
+            else:
+                positional_arguments.append(args[index].strip())
+
+        return positional_arguments, kwargs, extra_kwargs
+
+    def parse_message(self, message: Message):
         if not self.commands:
-            return
+            return None, []
 
         if not message.content.startswith(self.command_prefix):
-            return
+            return None, []
 
         no_prefix = self.remove_prefix(message.content)  # The content of the message but without the command_prefix
 
@@ -40,12 +69,10 @@ class CommandParser:
 
         command = self.commands.get(no_prefix.split()[0])
         if command:
-            args = no_prefix[len(command.name) :].split()
-
-            return command, args
+            return command, *self.get_args(command, no_prefix)
 
         for regex_command in filter(
-            lambda cmd: cmd.is_regex_command, self.commands.values()
+                lambda cmd: cmd.is_regex_command, self.commands.values()
         ):
             regex = regex_command.name
             regex_match_func = regex_command.regex_match_func
@@ -59,7 +86,6 @@ class CommandParser:
                         "First match group of command regex does not exist"
                     )
 
-                args = no_prefix[len(prefix) :].split()
-                return regex_command, args
+                return regex_command, *self.get_args(command, no_prefix, prefix=prefix)
 
-        return (None, [])
+        return None, []
