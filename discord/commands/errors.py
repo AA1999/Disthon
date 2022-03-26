1391150748d3d32c370599186224d089ba189cbd
Rashaad Akbar <rushda2000@gmail.com>
2022-03-26 13:51:31 +0530

    fix: Added CommandNotFound error

--- discord/commands/errors.py
+++ discord/commands/errors.py
@@ -2,7 +2,14 @@ class CommandError(Exception):
     pass
 
 
-class CheckFailure(Exception):
+class CheckFailure(CommandError):
     def __init__(self, command):
         self.command = command
         super().__init__(f"Check failed for command {command.name}")
+
+
+class CommandNotFound(CommandError):
+    def __init__(self, command_name):
+        self.command_name = command_name
+
+        super().__init__(f"Command with the name {self.command_name} does not exist")
